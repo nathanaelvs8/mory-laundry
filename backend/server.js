1eea8ccb73cc8920cccdac0afa1b8b3d7f7f92cd
natzyosu <nath02vs@gmail.com>
2025-12-19 19:53:01 +0700

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
